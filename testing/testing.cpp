0bdfd1e973c07f239c80ea2a80d2e754727b677c
toonsevrin <twan123@live.be>
2017-09-03 23:22:03 +0200

    Removed copy/paste bloat

--- testing/testing.cpp
+++ testing/testing.cpp
@@ -1,38 +1,3 @@
-/**
- *  @file exchange.cpp
- *  @brief defines an example exchange contract 
- *
- *  This exchange contract assumes the existence of two currency contracts
- *  located at @currencya and @currencyb.  These currency contracts have
- *  provided an API header defined in currench.hpp which the exchange 
- *  contract will use to process messages related to deposits and withdraws.
- *
- *  The exchange contract knows that the currency contracts requireNotice()
- *  of both the sender and receiver; therefore, the exchange contract can
- *  implement a message handler that will be called anytime funds are deposited
- *  to or withdrawn from the exchange.
- *
- *  When tokens are sent to @exchange from another account the exchange will
- *  credit the user's balance of the proper currency. 
- *
- *  To withdraw from the exchange, the user simply reverses the "to" and "from"
- *  fields of the currency contract transfer message. The currency contract will
- *  require the "authority" of the exchange, but the exchange's init() function
- *  configured this permission to allow *anyone* to transfer from the exchange.
- *
- *  To prevent people from stealing all the money from the exchange, the
- *  exchange's transfer handler  requires both the authority of the receiver and
- *  asserts that the user has a sufficient balance on the exchange. Lacking
- *  both of these the exchange will kill the transfer.
- *
- *  The exchange and one of the currency contracts are forced to execute in the same
- *  thread anytime there is a deposit or withdraw. The transaction containing
- *  the transfer are already required to include the exchange in the scope by
- *  the currency contract.
- *
- *  creating, canceling, and filling orders do not require blocking either currency
- *  contract.  Users can only deposit or withdraw to their own currency account.
- */
 #include <testing/testing.hpp> /// defines transfer struct
 #include <eoslib/print.hpp>
 
